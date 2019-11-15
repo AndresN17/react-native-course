8c0d63475099a60f8b38ab5118f67f3b3d32b80f
Andres Noboa <andresnoboa17@hotmail.com>
2019-11-15 15:16:57 -0500

    Creating Goals app with a ScrollView

--- App.js
+++ App.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import { StyleSheet, TextInput, View,Button } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, TextInput, View,Button,Text,ScrollView } from 'react-native';
 
 export default function App() {
+  const [enteredGoal,setEnteredGoal]=useState('');
+  const [courseGoals,setCourseGoals]=useState([]);
+
+  const goalInputHandler=(enteredText)=>{
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler=()=>{
+    setCourseGoals(currentGoals => [...courseGoals,enteredGoal]);
+  };
+
   return (
-    <View style={{padding:30}}>
-      <View style={{flexDirection: "row", justifyContent:"space-between", alignItems: "center"}}>
-        <TextInput placeholder="Course Goal." 
-        style={{ width: "80%" ,borderColor:"black", borderWidth:1}}/>
-        <Button title="add"/>
+    <View style={styles.screen}>
+      <View style={styles.top}>
+        <TextInput placeholder="Course Goal." style={styles.textInput} 
+        onChangeText={goalInputHandler} value={enteredGoal}/>
+        <Button title="add" onPress={addGoalHandler}/>
       </View>
-      <View></View>
+      <ScrollView>
+        {courseGoals.map((goal)=>
+        <View style={styles.listItem} key={goal}>
+          <Text>{goal}</Text>
+        </View>)}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  screen:{
+    padding:30,
+  },
+  top:{
+    flexDirection: "row", 
+    justifyContent:"space-between", 
+    alignItems: "center"
+  },
+  textInput:{
+    width: "80%" ,
+    borderColor:"black",
+    borderWidth:1
+  },
+  listItem:{
+    padding: 10,
+    marginVertical:10,
+    backgroundColor: "#ccc",
+    borderColor:"black",
+    borderWidth:1
+  }
   
 });

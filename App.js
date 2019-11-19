c392ccff63e7928711ccdada86776b7d0dad7441
Andres Noboa <andresnoboa17@hotmail.com>
2019-11-19 12:58:10 -0500

    Adding delete function in GoalItem

--- App.js
+++ App.js
@@ -10,15 +10,25 @@ export default function App() {
     setCourseGoals(currentGoals => [...currentGoals, { id: Math.random().toString(), value: goalTitle }]);
   };
 
+  const removeGoalHandler = (goalId) => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id != goalId)
+    });
+  }
+
   return (
     <View style={styles.screen}>
       <GoalInput onAddGoal={addGoalHandler} />
       <View>
         <FlatList
-          contentContainerStyle={{paddingBottom:20}}
+          contentContainerStyle={{ paddingBottom: 20 }}
           keyExtractor={(item, index) => item.id}
           data={courseGoals}
-          renderItem={itemData => (<GoalItem onDelete={()=>console.log("Quieres elimarlo?")} title={itemData.item.value} />)} />
+          renderItem={itemData => (
+            <GoalItem
+              id={itemData.item.id}
+              onDelete={removeGoalHandler}
+              title={itemData.item.value} />)} />
       </View>
     </View>
   );
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 
 const GoalItem = (props) => {
     return (
-        <TouchableWithoutFeedback onPress={props.onDelete}>
+        <TouchableOpacity activeOpacity={0.8} onPress={props.onDelete.bind(this,props.id)}>
             <View style={styles.listItem}>
                 <Text>{props.title}</Text>
             </View>
-        </TouchableWithoutFeedback>
+        </TouchableOpacity>
     );
 
 };

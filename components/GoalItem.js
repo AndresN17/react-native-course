9706ae4f75ec78dc90dcdf42c8f356a402a3ad7c
Andres Noboa <andresnoboa17@hotmail.com>
2019-11-19 12:16:58 -0500

    Using TouchableNativeFeedback only in Android Devices.

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableHighlight } from 'react-native';
+import { View, Text, StyleSheet, TouchableNativeFeedback } from 'react-native';
 
 
 const GoalItem = (props) => {
     return (
-        <TouchableHighlight onPress={props.onDelete}>
+        <TouchableNativeFeedback onPress={props.onDelete}>
             <View style={styles.listItem}>
                 <Text>{props.title}</Text>
             </View>
-        </TouchableHighlight>
+        </TouchableNativeFeedback>
     );
 
 };
